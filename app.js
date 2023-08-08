9c2d4275e6b8fd008dcfbebf3805f09d0b053fb8
Veronica Valdez <66653416+veronicajujuy@users.noreply.github.com>
2023-08-07 22:09:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ function Hola(camada) {
   console.log("Hola" + camada);
 }
 
-function Chau(camada) {
+function ChauChau(camada) {
   console.log("Chau" + camada);
 }
 
 Hola("Camada1");
-Chau("Camada1");
+ChauChau("Camada1");
